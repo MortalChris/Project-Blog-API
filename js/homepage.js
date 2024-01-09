b4709d4222de602a06a1fe0165f0d10402f375f1
Christian Valeri <mortlaxris@outlook.com>
2024-01-09 14:24:31 -0600

    commented out app and change app.use to homepage.use

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,10 @@
 //express
 const express = require('express')
-const app = express();
+// const app = express();
 const homepage = express.Router();
 // body parser
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+homepage.use(bodyParser.urlencoded({ extended: true }));
 
 homepage.get("/homepage", (req, res) => {
     res.render('homepage');
