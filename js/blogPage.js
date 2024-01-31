a16ca97177d65e8e10360c58f6ee28d461c4dbf4
Christian Valeri <mortlaxris@outlook.com>
2024-01-31 13:00:59 -0600

    grabs mongodb data for ejs

--- js/blogPage.js
+++ js/blogPage.js
@@ -4,12 +4,19 @@ const blogPage = express.Router();
 // body parser
 const bodyParser = require('body-parser');
 blogPage.use(bodyParser.urlencoded({ extended: true }));
-//server mongoose/ mongodb & bodyparse
-const mongoose = require('mongoose');
+// Import blog Model
+const BlogModel = require("./blogModelSchema");
 
-blogPage.get("/blogPage", async (req, res) => {
+blogPage.get("/blogPage/:id", async (req, res) => {
+    const blogId = req.params.id;
     try {
-        res.render('blogPage');
+        //Using model it finds the data in mongodb
+        const data = await BlogModel.find({}).exec();
+        // data is an array of objects, not a single object!
+        res.render('blogPage', {
+            title: req.title,   
+            data: data
+        });
     } catch (err) {
         console.error('Error fetching data from MongoDB:', err);
         res.status(500).send('Internal Server Error');
