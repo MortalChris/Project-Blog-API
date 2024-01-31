91fe205e877fe93744858ed61d755ec15877634d
Christian Valeri <mortlaxris@outlook.com>
2024-01-31 13:00:45 -0600

    homepage grabs data from mongodb for ejs

--- js/homepage.js
+++ js/homepage.js
@@ -5,9 +5,22 @@ const homepage = express.Router();
 // body parser
 const bodyParser = require('body-parser');
 homepage.use(bodyParser.urlencoded({ extended: true }));
+// Import blog Model
+const BlogModel = require("./blogModelSchema");
 
-homepage.get("/homepage", (req, res) => {
-    res.render('homepage');
+homepage.get("/homepage", async (req, res) => {
+    try {
+        //Using model it finds the data in mongodb
+        const data = await BlogModel.find({}).exec();
+        // data is an array of objects, not a single object!
+        res.render('homepage', {
+            title: req.title,
+            data: data
+        });
+    } catch (err) {
+        console.error('Error fetching data from MongoDB:', err);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 module.exports = homepage;  
