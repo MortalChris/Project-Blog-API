f39787c177a0b336bf97a418ac18d7687cccc8e7
Christian Valeri <mortlaxris@outlook.com>
2024-01-30 18:19:02 -0600

    moved model to a seperate file

---
+++ js/blogModelSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+//express
+const express = require('express')
+
+const BlogModel = mongoose.model('Blogs', new mongoose.Schema({
+    // Define your data schema here
+    title: { type: String, required: true},
+    body: { type: String, required: true }
+    // Add more fields as needed
+}));
+
+module.exports = BlogModel;
